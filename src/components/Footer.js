18742c7437bcf7e1fc6ca517c2e1270723911666
Cole Wilkison <wilkison.cole@gmail.com>
2021-05-11 14:03:59 -0400

    homepage layout done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,9 @@ import Fade from "react-reveal/Fade";
 
 const Footer = () => {
   return (
-    <footer className="bck_blue">
+    <footer className="footer-container">
       <Fade delay={500}>
-        <div className="font_righteous footer_logo_venue">
-          Tim Wilkison Partners
-        </div>
+        <div>Tim Wilkison Partners</div>
         <div className="footer_copyright">
           A Wilkison Production - 2021. All rights reserved.
         </div>
